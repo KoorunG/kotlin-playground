15cb540a0c644ecee20361e0b635866fb1f95396
KoorunGG <onlyzion10@naver.com>
2024-01-01 18:45:43 +0900

    feat: section6 - 코틀린의 runCatching을 이용한 다중예외 처리방법

--- src/test/kotlin/com/koorung/kotlinplayground/advancedkotlin/section7/tailrec/KotlinTryCatch.kt
+++ src/test/kotlin/com/koorung/kotlinplayground/advancedkotlin/section7/tailrec/KotlinTryCatch.kt
@@ -1,16 +1,56 @@
 package com.koorung.kotlinplayground.advancedkotlin.section7.tailrec
 
+import kotlin.reflect.KClass
+
 // Java와는 다르게 Kotlin에서는 여러 예외를 처리하는것이 불가능하다
-// when expression을 사용하여 우회하자
+// 1. when expression을 사용하여 우회하거나
+// 2. runCatching + 예외처리용 boiler plate을 이용하면 된다. (복잡)
 
-class AException: RuntimeException()
-class BException: RuntimeException()
-class CException: RuntimeException()
+class AException : RuntimeException()
+class BException : RuntimeException()
+class CException : RuntimeException()
 
-fun throwsException(n: Int) {
+// param에 따라 무조건 예외를 던지는 함수...
+fun logic(n: Int) {
     when {
         n > 0 -> throw AException()
         n == 0 -> throw BException()
         else -> throw CException()
     }
+}
+
+// --------------- 예외처리를 위한 boiler plate --------------- //
+
+// 상위의 예외가 아래로 내려오지 않게 하기 위한 Wrapper
+class ResultWrapper<T>(
+    private val result: Result<T>,
+    private val knownExceptions: MutableList<KClass<out Throwable>>
+) {
+    fun toResult(): Result<T> = this.result
+    fun onError(vararg exceptions: KClass<out Throwable>,
+                action: (Throwable) -> Unit): ResultWrapper<T> {
+        this.result.exceptionOrNull()?.let {
+            if(it::class in exceptions && it::class !in this.knownExceptions) {
+                action(it)
+            }
+        }
+        return this
+    }
+}
+
+// runCatching의 결과로 리턴되는 Result<T> 타입에 확장함수를 선언함
+// 파라미터로 예외 타입토큰을 vararg로 받고
+// 람다로 예외를 잡았을 시 실행할 action 을 받는다.
+// ResultWrapper<T>이라는 Wrapper으로 감싸서 반환하는데
+// 여기에서 잡힌 예외가 하위블록에서 다시 잡히지 않게 하기 위함이다..!
+// ResultWrapper<T>의 onError()과 확장함수의 onError() 모두 ResultWrapper<T>를 던지기 때문에
+// 메소드 체이닝이 가능하다!
+fun <T> Result<T>.onError(vararg exceptions: KClass<out Throwable>,
+                          action: (Throwable) -> Unit): ResultWrapper<T> {
+    exceptionOrNull()?.let {
+        if (it::class in exceptions) {
+            action(it)
+        }
+    }
+    return ResultWrapper(this, exceptions.toMutableList())
 }
--- src/test/kotlin/com/koorung/kotlinplayground/advancedkotlin/section7/tailrec/Tests.kt
+++ src/test/kotlin/com/koorung/kotlinplayground/advancedkotlin/section7/tailrec/Tests.kt
@@ -36,7 +36,26 @@ class Tests {
     @Test
     fun `Id를 인라인 제네릭 클래스로 만들어 구분짓기`() {
         // 인라인 클래스로 구현하면 컴파일단계에서 검증할 수 있다..!
-        val handle = handle(Id<User>(3L), Id<Book>(5L))
+        val userId = Id<User>(3L)
+        val bookId = Id<Book>(5L)
+        val handle = handle(userId, bookId)
         assertThat(handle).isEqualTo("userId :: 3, bookId :: 5")
+    }
+
+    @Test
+    fun `runCatching을 이용한 다중 예외처리`() {
+        val result = runCatching { logic(10) }
+            .onError(AException::class, BException::class) {
+                println("A 또는 B 예외가 발생했습니다.")
+            }
+            .onError(AException::class) {
+                println("위에서 예외가 잡혔기 때문에 내려오지 않음..")
+            }
+            .onError(CException::class) {
+
+            }
+            .toResult()
+
+        assertThat(result.isFailure).isTrue()
     }
 }
